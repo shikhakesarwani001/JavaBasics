01e99f741d0b8a2ef73677d79e4eaac2c2f7f783
Shikha <Shikha.Kesarwani@philips.com>
2022-06-22 09:55:27 +0530

    star pattern optimized

--- PrintPattern/Starpattern.java
+++ PrintPattern/Starpattern.java
@@ -1,23 +1,20 @@
 public class Starpattern {
-    //   *
-    //  ***
-    // *****
-    //*******
+    //      *
+    //    * *
+    //  * * *
+    //* * * * 
     public static void main(String args[]) {
-      
-        for(int i=1; i<=4;i++){
-            
-           for(int s=4; s>=i; s--){
-            System.out.print(" ");
-          }
-          for(int t=1;t<=i;t++){
-              System.out.print("*");
-          }
-          for(int sp=2;sp<=i;sp++)
-          {
-              System.out.print("*");
-          }
-          System.out.println();
+      int n=4;
+      for(int i=1; i<=n;i++){
+        for(int j=1;j<=n;j++){
+            if(j<=n-i){
+                System.out.print("  ");
+            }
+            else{
+                System.out.print("* ");
+            }
         }
+        System.out.println();
+    }
       }
 }
