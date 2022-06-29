1f1a74c5065412b5cf80da2ef8bb6a7feee7967d
Shikha <shikhalearning@gmail.com>
2022-06-29 20:27:00 +0530

    ZigZag Pattern

---
+++ PrintPattern/Zig_ZagPattern.java
@@ -0,0 +1,23 @@
+package PrintPattern;
+
+public class Zig_ZagPattern {
+    //  *   *
+    // * * *  *
+    //*   *    *
+    public static void main(String args[]){
+
+        int n=9;
+        for(int i=1;i<=3;i++){
+
+            for(int j=1;j<=n;j++){
+                if((i+j)%4==0 || (i==2 && j%4==0 )){
+                        System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
