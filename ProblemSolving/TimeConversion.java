15200e0f008b194331ab80c73ce0cfc12382fa29
Shikha <shikhalearning@gmail.com>
2022-07-13 00:00:30 +0530

    fix

--- ProblemSolving/TimeConversion.java
+++ ProblemSolving/TimeConversion.java
@@ -37,16 +37,7 @@
 package ProblemSolving;
 
 import java.io.*;
-import java.math.*;
-import java.security.*;
-import java.text.*;
-import java.util.*;
-import java.util.concurrent.*;
-import java.util.function.*;
-import java.util.regex.*;
-import java.util.stream.*;
-import static java.util.stream.Collectors.joining;
-import static java.util.stream.Collectors.toList;
+
 
 
 class Result {
@@ -89,16 +80,16 @@ class Result {
 public class TimeConversion {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+        //BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
 
         String s = bufferedReader.readLine();
 
         String result = Result.timeConversion(s);
 
-        bufferedWriter.write(result);
-        bufferedWriter.newLine();
+        //bufferedWriter.write(result);
+        //bufferedWriter.newLine();
 
         bufferedReader.close();
-        bufferedWriter.close();
+        //bufferedWriter.close();
     }
 }
